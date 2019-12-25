a1d4cc36e3049816f6093e90e4d0f0bacbca0d60
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-12-25 09:20:47 +0100

    added permissions handling

--- app/src/main/java/com/example/testzxing/MainActivity.kt
+++ app/src/main/java/com/example/testzxing/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.testzxing
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity: AppCompatActivity(), ZXingScannerView.ResultHandler{
+    private val REQUEST_CAMERA_PERMISSIONS = 1
     private lateinit var mScannerView: ZXingScannerView
 
     override fun onCreate(savedInstanceState: Bundle?){
@@ -17,16 +22,11 @@ class MainActivity: AppCompatActivity(), ZXingScannerView.ResultHandler{
 
     override fun onResume(){
         super.onResume()
-        startScanner()
+        requestPermissions()
     }
 
-    override fun onPause(){
+    override fun onPause() {
         super.onPause()
-        stopScanner()
-    }
-
-    override fun onDestroy(){
-        super.onDestroy()
         stopScanner()
     }
 
@@ -58,6 +58,31 @@ class MainActivity: AppCompatActivity(), ZXingScannerView.ResultHandler{
                 onClick{
                     startScanner()
                 }
+            }
+        }
+    }
+
+    private fun requestPermissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)){
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSIONS)
+            }
+        }else{
+            startScanner()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray){
+        when(requestCode){
+            REQUEST_CAMERA_PERMISSIONS -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+                    longToast("Permission granted")
+                }else{
+                    longToast("This application needs camera permissions to work correctly!")
+                }
+                return
+            }
+            else -> {
             }
         }
     }
